47300c7faa0d8e761713dc06a97a04a7b699b3e4
Mirela Damyanova <mdamyanova181@gmail.com>
2024-05-02 14:15:38 +0300

    Upload TownsDbScript.sql

---
+++ Courses/Software-Sciences/Module-4-Information-Systems-New/05-Connecting-Windows-Forms-with-database/Resources/TownsDbScript.sql
@@ -0,0 +1,34 @@
+﻿CREATE TABLE Towns (
+    Id INT PRIMARY KEY,
+    Name NVARCHAR(100)
+);
+
+INSERT INTO Towns (Id, Name)
+VALUES
+    (1, 'Blagoevgrad'),
+    (2, 'Burgas'),
+    (3, 'Varna'),
+    (4, 'Veliko Tarnovo'),
+    (5, 'Vidin'),
+    (6, 'Vratsa'),
+    (7, 'Gabrovo'),
+    (8, 'Dobrich'),
+    (9, 'Kardzhali'),
+    (10, 'Kyustendil'),
+    (11, 'Lovech'),
+    (12, 'Montana'),
+    (13, 'Pazardzhik'),
+    (14, 'Pleven'),
+    (15, 'Pernik'),
+    (16, 'Plovdiv'),
+    (17, 'Razgrad'),
+    (18, 'Ruse'),
+    (19, 'Silistra'),
+    (20, 'Sliven'),
+    (21, 'Smolyan'),
+    (22, 'Sofia'),
+    (23, 'Stara Zagora'),
+    (24, 'Targovishte'),
+    (25, 'Haskovo'),
+    (26, 'Shumen'),
+    (27, 'Yambol');
