e0ad2f0536a30e025153384a755f27e7576fce10
Mirela Damyanova <mdamyanova181@gmail.com>
2024-06-10 11:34:33 +0300

    Update TownsDbScript.sql

--- Courses/Software-Sciences/Module-4-Information-Systems-New/05-Connecting-Windows-Forms-with-database/Resources/TownsDbScript.sql
+++ Courses/Software-Sciences/Module-4-Information-Systems-New/05-Connecting-Windows-Forms-with-database/Resources/TownsDbScript.sql
@@ -1,34 +1,41 @@
-﻿CREATE TABLE Towns (
+﻿CREATE DATABASE TownsDb;
+GO
+
+USE TownsDb;
+GO
+
+CREATE TABLE Towns (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL
 );
+GO
 
-INSERT INTO Towns (Name)
-VALUES
-    ('Blagoevgrad'),
-    ('Burgas'),
-    ('Varna'),
-    ('Veliko Tarnovo'),
-    ('Vidin'),
-    ('Vratsa'),
-    ('Gabrovo'),
-    ('Dobrich'),
-    ('Kardzhali'),
-    ('Kyustendil'),
-    ('Lovech'),
-    ('Montana'),
-    ('Pazardzhik'),
-    ('Pleven'),
-    ('Pernik'),
-    ('Plovdiv'),
-    ('Razgrad'),
-    ('Ruse'),
-    ('Silistra'),
-    ('Sliven'),
-    ('Smolyan'),
-    ('Sofia'),
-    ('Stara Zagora'),
-    ('Targovishte'),
-    ('Haskovo'),
-    ('Shumen'),
-    ('Yambol');
+INSERT INTO Towns (Name) VALUES
+(N'Благоевград'),
+(N'Бургас'),
+(N'Варна'),
+(N'Велико Търново'),
+(N'Видин'),
+(N'Враца'),
+(N'Габрово'),
+(N'Добрич'),
+(N'Кърджали'),
+(N'Кюстендил'),
+(N'Ловеч'),
+(N'Монтана'),
+(N'Пазарджик'),
+(N'Плевен'),
+(N'Перник'),
+(N'Пловдив'),
+(N'Разград'),
+(N'Русе'),
+(N'Силистра'),
+(N'Сливен'),
+(N'Смолян'),
+(N'София'),
+(N'Стара Загора'),
+(N'Търговище'),
+(N'Хасково'),
+(N'Шумен'),
+(N'Ямбол');
+GO
